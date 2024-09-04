a092bc88648d064afca2a8f3a35641ba284f78c4
MinKhantYCC <linyaungzin99@gmail.com>
2024-09-04 21:10:18 +0700

    dynamic urls

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Welcome, Challenger!")
+
+def monthly_challenge(request, month):
+    # month is identifier assigned in urls.py
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Don't Eat Sugar"
+    elif month == "february":
+        challenge_text = "Walk 1 hour"
+    elif month == "march":
+        challenge_text = "Exercise 1 hour"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
